36404a66a161b986b810187b9c98dc8075b7f0ca
victoriamata <victoriamataxo@gmail.com>
2024-10-16 21:34:52 -0400

    Corrected a few errors and updated the README.md file

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -374,8 +374,20 @@ class Cli {
           }
         }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        else if (answers.action === 'Tow') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Truck) {
+                this.findVehicleToTow();
+              }
+            }
+          }
+          return;
+        }
+
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         else if (answers.action === 'Select or create another vehicle') {
+          if (this.vehicles instanceof Motorbike) 
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
           return;
--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -47,7 +47,7 @@ class Motorbike extends Vehicle {
   
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-if (){
+if () {
   console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
 }
 
